5897c08eb36643efac8a645cddc8890cb17ccc6b
dodevs <douglass.sousa@outlook.com.br>
2018-02-01 12:15:51 -0200

    Adição do express-validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,6 +6,22 @@ module.exports = function(app){
     app.post('/noticias/salvar', function(req, res){
         var noticia = req.body;
 
+
+        //Verificar a documentação do express-validator
+        req.assert('titulo', 'Título obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10,100);
+        req.assert('autor', 'Autor é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatória').notEmpty();
+        req.assert('noticia', 'Noticia é obrigatória').notEmpty();
+
+        var erros = req.validationErrors();
+
+        if(erros){
+            res.render("admin/form_add_noticia");
+            return;
+        }
+
         const connection = app.config.dbConnection();
         var noticiasModel = new app.app.models.NoticiasDAO(connection);
 
